f1048e95f3fe0ae6480969e6948d8b4a912cb925
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-16 17:50:55 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,12 @@
 class ChattyStudent < Student 
-  
-  
-  
+  def hello
+    super 
+    puts "Hey there! I'm so excited to learn stuff."
+    
+    super 
+    puts 
+  "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end 
   
   
   
--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
     
   end 
   
-  
+  def raise_hand 
+    puts "Pick me!"
+  end 
   
   
   
