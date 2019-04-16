37b48a16b96b8aafd2b2d1a4b109c957fed0b2d9
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-16 23:34:55 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,14 +1,17 @@
 class ChattyStudent < Student 
   def hello
-    super 
-    puts "Hey there! I'm so excited to learn stuff."
+     super 
     
-    super 
-    puts 
-  "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
-  
-  
+  def raise_hand 
+      super 
+      counter = 0
+      until counter == 10 
+      puts "Pick me!"
+      counter += 1 
+    end 
+  end 
   
 end 
