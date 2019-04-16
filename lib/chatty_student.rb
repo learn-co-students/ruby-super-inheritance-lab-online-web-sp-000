0de1bd017b544ff0f7d00a67014a072cf34d50b3
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-16 14:58:30 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,9 @@
+class ChattyStudent < Student 
+  
+  
+  
+  
+  
+  
+  
+end 
---
+++ lib/student.rb
@@ -0,0 +1,13 @@
+class Student 
+  
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+    
+  end 
+  
+  
+  
+  
+  
+  
+end 
