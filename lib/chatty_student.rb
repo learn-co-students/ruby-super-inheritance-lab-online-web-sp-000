e17f039f21900ea17b2d4e9045ff05689a16ac81
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-24 16:03:39 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,9 +3,17 @@ class ChattyStudent < Student
 
   def hello
     super
-    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't? Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    #puts "test text"
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
+  def raise_hand
+    super
+    9.times do
+      puts "Pick me!"
+    end
+
+    end
 
 
 end
