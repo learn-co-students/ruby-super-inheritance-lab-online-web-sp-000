613390c114887a5ab6c262ba4661bea6fc4e3b73
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 16:05:57 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,11 @@
-class ChattyStudent
+
+require_relative "./student.rb"
+class ChattyStudent < Student
   def hello
-    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
 
-  def raise_hand
-    10.times do
-      puts "Pick me!\n"
-    end
-  end
+
+
 end
