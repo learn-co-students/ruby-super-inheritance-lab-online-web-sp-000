3f1483b8e0484365137a1851eaa8b082dc4b89ba
yaoyu bao <yaobao5354@gmail.com>
2019-09-05 23:45:34 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,19 @@
-class ChattyStudent
+class ChattyStudent < Student
   
   
   def hello
-    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end 
   
   def raise_hand 
-    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+    counter = 0
+    while counter < 10 
+      super
+      counter +=1 
+    end 
+     
+    
   end 
+  
 end 
