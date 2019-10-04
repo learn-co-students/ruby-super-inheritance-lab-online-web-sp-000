c97397bc38864aa5f1c78407b06b7078bdb259ae
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-04 00:43:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
 
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    "Pick me!"
+    puts "Pick me!"
   end
-  
+
 end
