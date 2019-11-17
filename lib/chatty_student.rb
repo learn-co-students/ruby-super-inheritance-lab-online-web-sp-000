f4c30496a8c8dbe1820b02a3d25d018105342cc4
Dr. XyClo <sdcarmody@gmail.com>
2019-11-17 03:01:22 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,31 @@
+class ChattyStudent < Student
+  
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+  
+  def raise_hand
+    super
+    
+    i = 0
+    while i < 9  do
+      puts "Pick me!"
+      i +=1
+      end
+    end
+  
+  # alt super
+  #     super
+  #     super
+  #     super
+  #     super
+  #     super
+  #     super
+  #     super
+  #     super
+  #     super
+
+  
+  
+end
