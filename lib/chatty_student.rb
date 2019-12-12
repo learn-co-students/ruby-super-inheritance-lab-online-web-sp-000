285b8bc6bd40ffdf1098f97f043fc1c54bc63ec8
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-12 17:06:35 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,7 +3,7 @@ require_relative "./student.rb"
 class ChattyStudent < Student
 
   def hello
-    super
+  puts   super
      "How are you doing today? I'm okay, but I'm kind of tired.
  Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\
 n"
