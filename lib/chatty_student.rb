39c8fa620d83a05581877d96e879f86a29aa51d1
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-12 16:57:45 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,17 @@
+require_relative "./student.rb"
+
+class ChattyStudent < Student
+
+  def hello
+    super
+     "How are you doing today? I'm okay, but I'm kind of tired.
+ Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\
+n"
+  end
+
+  def raise_hand
+    super * 10
+  end
+
+
+end
---
+++ lib/student.rb
@@ -0,0 +1,9 @@
+class Student
+  def hello
+    "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+     "Pick me!" * 10
+  end
+end
