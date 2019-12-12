a5355d08c1bc4e1f11eb5b75c7be91d6796ddb3c
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-12 17:50:20 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,17 +1,15 @@
 require_relative "./student.rb"
-
+require "pry"
 class ChattyStudent < Student
 
   def hello
-  puts   super
-     "How are you doing today? I'm okay, but I'm kind of tired.
- Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\
-n"
+    super
+  puts  "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
   def raise_hand
-    super * 10
+    10.times do super
+      end
   end
-
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 class Student
   def hello
-    "Hey there! I'm so excited to learn stuff."
+  puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-     "Pick me!" * 10
+  puts "Pick me!"
   end
 end
