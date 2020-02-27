10e6a48f2ec475f9ee0226c61a4de53d39bc7320
JibJames <masterbridges@gmail.com>
2020-02-27 16:40:03 -0700

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,10 @@
+require_relative "./student.rb"
+class ChattyStudent < Student
+    def hello
+        puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    end
+
+    def raise_hand
+        10.times{puts "Pick me!"}
+    end
+end
