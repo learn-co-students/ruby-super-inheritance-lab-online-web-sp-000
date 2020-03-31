abf4b1b151c6e41d13a32f9364ea194c9d1d0606
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 21:54:32 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,10 @@
+class Student
+  def hello
+    puts phrase =  "Hey there! I'm so excited to learn stuff."#,"How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+  def raise_hand
+    phrase = 'Pick me!'
+    puts phrase
+    #  10.times { puts phrase }
+  end
+end
