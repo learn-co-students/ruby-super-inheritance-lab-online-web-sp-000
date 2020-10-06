f72ab862c73610c3fdbf92bb94d8277619d5ced1
McKayla Lankau <lankaukk@yahoo.com>
2020-10-06 05:23:14 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,18 +5,10 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
-  def raise_hand 
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    
+  def raise_hand
+    10.times do
+      super
+    end
   end
   
 end
