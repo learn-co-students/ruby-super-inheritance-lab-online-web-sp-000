82b836dbda44c7242eedfa1415f3e14c5e94d6ed
Ryan K Burns <ryankburns@gmail.com>
2020-10-13 17:45:50 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,12 +2,21 @@ class ChattyStudent < Student
 
   def hello
     super
-    puts "How are you doing today? I'm okay. but I'm kind of tired/ Did you watch the Walking Dead last night? You didn't?! Oh man, it was crazy! What, you don't want any spoilers? Okay well let me just tell you who dies..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
   def raise_hand
     super
-    puts "Pick me!".times(10)
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   def hello
-    puts "hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
