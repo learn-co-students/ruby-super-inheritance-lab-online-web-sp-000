7e21534763877c749d508280d5a111b9e328e10d
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 03:27:33 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,11 @@
+class ChattyStudent < Student
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+
+  def raise_hand
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!"
+  end
+
+end
