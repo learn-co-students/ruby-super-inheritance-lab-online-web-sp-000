e7765fd9ba84e4aeb9c65c23125c7130c29c924e
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-09 21:10:11 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Chatty_student
+  
+  def hello 
+    super(message)
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end 
+  
+  def raise_hand 
+    times do 10
+    super 
+    puts "Pick me!"
+    end 
+end 
---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Student
+  
+  
+  def hello(message)
+    puts "Hey there!  I'm so excited to learn stuff."
+  end 
+  
+  def raise_hand(phrase)
+    puts "Pick me!"
+  end  
+end 
+  
