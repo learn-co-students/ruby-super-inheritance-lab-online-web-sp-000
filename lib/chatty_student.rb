ee50917509fd9a3204368fa935e2330250101b01
Milan Mody <milanm1120@gmail.com>
2021-03-24 00:15:27 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,21 @@
+require_relative "./student.rb"
+
+class ChattyStudent < Student
+
+  def hello
+    super
+      puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+
+  def raise_hand
+    10.times do
+      puts super
+    end
+  end
+
+end
+
+
+s1 = ChattyStudent.new
+s1.hello
+s1.raise_hand
